c966993b8a31759722aafcce214879efb8de265c
Igor Mokhov <igormokhovid@gmail.com>
2025-10-18 14:18:28 +0700

    add Home

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,7 +1,23 @@
+import Title from 'antd/es/typography/Title';
+import { Button } from 'antd';
+import { removeToken } from '../../utils/auth';
+import { useNavigate } from 'react-router-dom';
+import styles from './style.module.css';
+
 export const Home = () => {
+  const navigate = useNavigate();
+
+  const onLogoutHandler = () => {
+    removeToken();
+    navigate('/login');
+  };
+
   return (
-    <main>
-      <h1>Hello World!</h1>
-    </main>
+    <section className={styles.home}>
+      <Title level={1}>Hello World!</Title>
+      <Button type="primary" onClick={onLogoutHandler}>
+        Log out
+      </Button>
+    </section>
   );
 };
